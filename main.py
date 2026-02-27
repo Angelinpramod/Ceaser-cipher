cadc2461e335880b4b3fb2cb897f86027014fed2
Angelinpramod <angelinpramod06@gmail.com>
2026-02-27 22:02:06 +0530

    V3:Bruteforce + file features

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 #basic caser cipher
-
-dict={0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X',24:'Y',25:'Z'}
+import argparse
+dic={0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X',24:'Y',25:'Z'}
 
 def encrypt(text, shift):
     cipher = ""
     for i in text:
         if i.isalpha():
-            for j in dict:
-                if i.upper() == dict[j]:
+            for j in dic:
+                if i.upper() == dic[j]:
                     pos = (j + shift) % 26
                     if i.islower():
-                        cipher += dict[pos].lower()
+                        cipher += dic[pos].lower()
                     else:
-                        cipher += dict[pos]
+                        cipher += dic[pos]
         else:
             cipher += i
     return cipher
@@ -22,25 +22,44 @@ def decrypt(text, shift):
     cipher = ""
     for i in text:
         if i.isalpha():
-            for j in dict:
-                if i.upper() == dict[j]:
+            for j in dic:
+                if i.upper() == dic[j]:
                     pos = (j - shift) % 26
                     if i.islower():
-                        cipher += dict[pos].lower()
+                        cipher += dic[pos].lower()
                     else:
-                        cipher += dict[pos]
+                        cipher += dic[pos]
         else:
             cipher += i
     return cipher
 
+parser = argparse.ArgumentParser(description='Caesar Cipher')
+parser.add_argument('--mode', choices=['encrypt', 'decrypt','bruteforce'], required=True, help='Mode of operation: encrypt, decrypt or bruteforce')
+parser.add_argument('--shift', type=int, required=False, default=0, help='Shift value for the cipher (default: 0)')
+parser.add_argument('--text', type=str, required=False, default=None, help='Text to encrypt or decrypt')
+parser.add_argument('--file', type=str, required=False, help='Path to input file')
+
 def main():
-    choice=int(input("1.Encrpyt\n2.Decrypt\nEnter your choice:"))
-    text=input("Enter the text:")
-    shift=int(input("Enter the shift:"))
-    if choice==1:
-        print("Encrypted text:",encrypt(text,shift))
-    elif choice==2:
-        print("Decrypted text:",decrypt(text,shift))
+    args = parser.parse_args()
+    text = args.text
+    shift = args.shift
+    file=args.file
+    if file:
+        try:
+            with open(file, 'r') as f:
+                text = f.read()
+        except FileNotFoundError:
+            print(f"Error: File '{file}' not found.")
+            return
+    if args.mode == "encrypt":
+        print("Encrypted text:", encrypt(text, shift))
+    elif args.mode == "decrypt":
+        print("Decrypted text:", decrypt(text, shift))
+    elif args.mode == "bruteforce":
+        for shift in range(1, 26):
+            print(f"Shift {shift}: {decrypt(text, shift)}")
+
+
 
 if __name__=="__main__":
     main()
