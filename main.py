6347d794795738ec64d5aa65f72b9fd4bd4dbba3
Angelinpramod <angelinpramod06@gmail.com>
2026-02-27 21:18:37 +0530

    V2: Case sensitive

--- main.py
+++ main.py
@@ -2,22 +2,35 @@
 
 dict={0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X',24:'Y',25:'Z'}
 
-def encrypt(text,shift):
-    cipher=""
+def encrypt(text, shift):
+    cipher = ""
     for i in text:
-        for j in dict:
-            if i.upper()==dict[j]:
-                pos=(j+shift)%26
-                cipher+=dict[pos]
+        if i.isalpha():
+            for j in dict:
+                if i.upper() == dict[j]:
+                    pos = (j + shift) % 26
+                    if i.islower():
+                        cipher += dict[pos].lower()
+                    else:
+                        cipher += dict[pos]
+        else:
+            cipher += i
     return cipher
 
-def decrypt(text,shift):
-    cipher=""
+
+def decrypt(text, shift):
+    cipher = ""
     for i in text:
-        for j in dict:
-            if i.upper()==dict[j]:
-                pos=(j-shift)%26
-                cipher+=dict[pos]
+        if i.isalpha():
+            for j in dict:
+                if i.upper() == dict[j]:
+                    pos = (j - shift) % 26
+                    if i.islower():
+                        cipher += dict[pos].lower()
+                    else:
+                        cipher += dict[pos]
+        else:
+            cipher += i
     return cipher
 
 def main():
